e807657430fbc7fea3386bd8893c63fa5ea82184
Prasanna Achar <acharprasanna5@gmail.com>
2025-02-22 08:19:17 +0530

    Chanllenge 3 Completed

--- JS DOM/challenge-3/app.js
+++ JS DOM/challenge-3/app.js
@@ -1,3 +1,26 @@
 /**
  * Write your challenge solution here
  */
+function dynamicChange(place, input){
+    place.innerHTML = input || "Not Provided"
+}
+const nameInput = document.getElementById('nameInput')
+const jobInput = document.getElementById('jobInput')
+const ageInput = document.getElementById('ageInput')
+const bioInput = document.getElementById('bioInput')
+
+nameInput.addEventListener('input', (e)=>{
+    dynamicChange(document.getElementById('nameDisplay'), e.target.value)
+})
+
+jobInput.addEventListener('input', (e) =>{
+    dynamicChange(document.getElementById('jobDisplay'), e.target.value)
+})
+
+ageInput.addEventListener('input', (e) =>{
+    dynamicChange(document.getElementById('ageDisplay'), e.target.value)
+})
+
+bioInput.addEventListener('input', (e) =>{
+    dynamicChange(document.getElementById('bioDisplay'), e.target.value)
+})
